93e98e5a8461b9155bf45d84ca0b45b05fbcf71b
Rakuraku Jyo <jyo.rakuraku@gmail.com>
2012-11-11 00:03:28 +0900

    v1 done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static(__dirname + '/public'));
 // app.use(express.logger());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.use(express.bodyParser());
 
 // Start a game with specified board, the game_id will be retuned
 app.get('/api/create/:board', function(req, res) {
@@ -19,25 +20,36 @@ app.get('/api/list', function(req, res) {
   res.json(l.listGames());
 });
 
-// To call the following APIs, game_id and client_id should be put in the header
+// To call the following APIs, game_id and player_id should be put in the header
 // x-header-lpb-game_id:game_id_hash
-// x-header-lpb-client_id:client_id_hash
+// x-header-lpb-player_id:player_id_hash
 function checkAuth(req, res, next) {
   var gameId = req.headers['x-lpb-game_id'];
-  var clientId = req.headers['x-lpb-client_id'];
-  if (!gameId || !clientId) {
-    res.send(401);
+  var playerId = req.headers['x-lpb-player_id'];
+
+  if (!gameId || !playerId) {
+    res.send(406, {error: 'Game ID and Client ID should be specified in the request header'});
   } else {
+    req.game = new l.Game(gameId, playerId)
     next();
   }
 }
 
+function checkJoined(req, res, next) {
+  var currentGame = req.game.game();
+  if (!currentGame) {
+    return res.send(404, {error: 'Game ID not available' });
+  } else if (currentGame.players.indexOf(req.game.playerId) == -1) {
+    return res.send(404, {error: 'You haven\'t joined this game.' });
+  } else if (currentGame.status == 0) {
+    return res.send(406, {error: 'Game not have enough players to start' });
+  }
+  next();
+}
+
 // Join a game
 app.get('/api/join', checkAuth, function(req, res) {
-  var gameId = req.headers['x-lpb-game_id'];
-  var clientId = req.headers['x-lpb-client_id'];
-
-  if (l.joinGame(gameId, clientId)) {
+  if (req.game.join()) {
     res.send(200);
   } else {
     res.send(404);
@@ -45,17 +57,40 @@ app.get('/api/join', checkAuth, function(req, res) {
 });
 
 // Input a move
-app.get('/api/move/:move', function(req, res) {
-  console.log(req.route);
-  res.json(content);
+app.get('/api/move/:move?', checkAuth, checkJoined, function(req, res) {
+  if (!req.params.move) {
+    res.json(req.game.lastMove());
+  } else {
+    var move = req.params.move.split(',');
+    if (move.length > 0) {
+      if (req.game.move(move)) {
+        return res.send(200);
+      }
+    }
+
+    res.send(406, {error: 'Invalid move [' + move + ']'});
+  }
 });
 
 // Resign a game
-app.get('/api/resign', function(req, res) {
-  console.log(req.route);
-  res.json(content);
+app.get('/api/resign', checkAuth, checkJoined, function(req, res) {
+  if (req.game.resign()) {
+    res.send(200);
+  } else {
+    res.send(406, {error: 'Unable to resign game. It\'s not your turn or you are not one of the players.'});
+  }
 });
 
+// Fetch a game
+app.get('/api/show', checkAuth, checkJoined, function(req, res) {
+  res.send(req.game.game())
+});
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+  res.send(404);
+});
+
+app.listen(3000);
+
 module.exports = app;
-// app.listen(3000);
-// console.log('Letterpress Battle Server Started.')
--- test/test_server.js
+++ test/test_server.js
@@ -1,15 +1,16 @@
 var should = require('should')
   , request = require('supertest')
-  , step = require('step')
   , app = require('../app');
 
 // Infomation
 // http://stackoverflow.com/questions/11927196/supertest-custom-express-server-in-node
 // https://github.com/visionmedia/supertest
 
-describe('User API',function(){
+var gameId;
 
-  it('GET /api/create/:board should return 200',function(done){
+describe('User API', function(){
+
+  it('GET /api/create/:board should return 200', function(done){
     request(app)
       .get('/api/create/testboard')
       .expect('Content-Type', /json/)
@@ -20,7 +21,7 @@ describe('User API',function(){
       });
   });
 
-  it('GET /api/list should return 200',function(done){
+  it('GET /api/list should return 200', function(done){
     request(app)
       .get('/api/list')
       .expect('Content-Type', /json/)
@@ -28,45 +29,130 @@ describe('User API',function(){
       .end(function(err, res) {
         var list = res.body;
         list.should.be.an.instanceOf(Array);
+        gameId = list[0];
         if (!err) done();
       });
   });
 
-  it('GET /api/join/:game_id without header should return 401',function(done){
+  // Precondition Failed
+  it('GET /api/join/:game_id without header should return 406', function(done){
     request(app)
       .get('/api/join')
-      .expect(401, done);
+      .expect(406, done);
   });
 
-  it('GET /api/join/:game_id should return 404',function(done){
+  it('GET /api/join/:game_id should return 404', function(done){
     request(app)
       .get('/api/join')
-      .set('x-lpb-client_id', 'CLIENT_ID')
+      .set('x-lpb-player_id', 'PLAYER1_ID')
       .set('x-lpb-game_id', 'abc')
       .expect(404, done);
   });
 
-  it('GET /api/join/:game_id should return 200',function(done){
-    step(
-      function() {
-        request(app)
-          .get('/api/list')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .end(this);
-      },
-      function(err, res) {
-        var list = res.body;
-        list.length.should.be.above(0);
-        var gameId = list[0];
+  it('GET /api/move should return 404', function(done){
+    request(app)
+      .get('/api/move')
+      .set('x-lpb-player_id', 'PLAYER1_ID')
+      .set('x-lpb-game_id', 'abc')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
+  });
 
-        request(app)
-          .get('/api/join')
-          .set('x-lpb-client_id', 'CLIENT_ID')
-          .set('x-lpb-game_id', gameId)
-          .expect(200, done);
-      }
-    );
+  it('GET /api/move/1,2,3 should return 404 before join the game', function(done){
+    var aMove = [1,2,3];
+    request(app)
+      .get('/api/move/' + aMove.join(','))
+      .set('x-lpb-player_id', 'PLAYER1_ID')
+      .set('x-lpb-game_id', gameId)
+      .expect(404, done);
+  });
+
+  it('GET /api/resign should return 404', function(done){
+    request(app)
+      .get('/api/resign')
+      .set('x-lpb-player_id', 'PLAYER1_ID')
+      .set('x-lpb-game_id', 'abc')
+      .expect(404, done);
+  });
+});
+
+describe('Game Logic', function(){
+  it('join a board', function(done){
+    request(app)
+      .get('/api/join')
+      .set('x-lpb-player_id', 'PLAYER1_ID')
+      .set('x-lpb-game_id', gameId)
+      .expect(200, done);
+  });
+
+  it('join a board by another player', function(done){
+    request(app)
+      .get('/api/join')
+      .set('x-lpb-player_id', 'PLAYER2_ID')
+      .set('x-lpb-game_id', gameId)
+      .expect(200, done);
+  });
+
+  it('get last move', function(done){
+    request(app)
+      .get('/api/move')
+      .set('x-lpb-player_id', 'PLAYER1_ID')
+      .set('x-lpb-game_id', gameId)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        res.body.should.be.instanceOf(Array);
+        res.body.length.should.eql(0);
+        done();
+      });
+  });
+
+  it('take a step', function(done){
+    var move = [1,2,3];
+    request(app)
+      .get('/api/move/' + move.join(','))
+      .set('x-lpb-player_id', 'PLAYER1_ID')
+      .set('x-lpb-game_id', gameId)
+      .expect(200, done);
+  });
+
+  it('take a step by the same player should fail', function(done){
+    var move = [1,2,3];
+    request(app)
+      .get('/api/move/' + move.join(','))
+      .set('x-lpb-player_id', 'PLAYER1_ID')
+      .set('x-lpb-game_id', gameId)
+      .expect(406, done);
+  });
+
+  it('take a step by another player', function(done){
+    var move = [1,2,3];
+    request(app)
+      .get('/api/move/' + move.join(','))
+      .set('x-lpb-player_id', 'PLAYER2_ID')
+      .set('x-lpb-game_id', gameId)
+      .expect(200, done);
+  });
+
+  it('resign the board', function(done){
+    request(app)
+      .get('/api/resign')
+      .set('x-lpb-player_id', 'PLAYER1_ID')
+      .set('x-lpb-game_id', gameId)
+      .expect(200, done);
+  });
+
+  it('check the board status', function(done){
+    request(app)
+      .get('/api/show')
+      .set('x-lpb-player_id', 'PLAYER1_ID')
+      .set('x-lpb-game_id', gameId)
+      .end(function(err, res) {
+        var game = res.body;
+        game.status.should.eql(2);
+        game.steps.length.should.eql(3);
+        done();
+      });
   });
 
 });
